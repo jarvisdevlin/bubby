4eb0774aaa617c3a7ee747f574a9a28fb541007f
jarvisdevlin <jarvisdevilyt@gmail.com>
2024-12-02 03:46:46 -0600

    Add files via upload

---
+++ README.md
@@ -0,0 +1,9 @@
+# bubby
+## Who is bubby?
+Bubby is your helper when it comes to everything Geometry Dash, learn more about your favorite game by clicking the bubby icon on the main menu.
+### Ok but who is bubby?
+Stupid nerd from diary of a wimpy kid LOL GET REKT BUBBY!!!
+### Why is it required to be online?
+I am not pushing 10,000 updates a day everytime I think of something good to add to the lines bubby is supposed to say. (generating bubby images in c++ sucks)
+### Wheres the list of stuff bubby says?
+You can find the list of stuff bubby says [right here](https://geometrydash.us.kg/pastes.php), I'll also post here once in a while.
---
+++ src/main.cpp
@@ -0,0 +1,96 @@
+#include <Geode/Geode.hpp>
+#include <Geode/modify/MenuLayer.hpp>
+#include <Geode/utils/web.hpp>
+#include <Geode/loader/Event.hpp>
+
+using namespace geode::prelude;
+
+class $modify(kanye, MenuLayer) {
+    // skidded button menu template AHHHHHHHHHHHH
+    struct Fields {
+        EventListener<web::WebTask> m_listener;
+        CCSprite* fetchedImage = nullptr;
+        CCMenuItemSpriteExtra* billy = nullptr;
+    };
+
+    bool init() {
+        if (!MenuLayer::init()) {
+            return false;
+        }
+
+        auto bubby = CCMenuItemSpriteExtra::create(
+            CCSprite::createWithSpriteFrameName("GJ_helpBtn_001.png"),
+            this,
+            menu_selector(kanye::onclick)
+        );
+
+        auto menu = this->getChildByID("bottom-menu");
+        menu->addChild(bubby);
+
+        bubby->setID("bubby2kool4u"_spr);
+        menu->updateLayout();
+
+        m_fields->billy = bubby;
+        return true;
+    }
+
+    void onclick(CCObject*) {
+        // trash code
+        m_fields->billy->setEnabled(false);
+
+        auto req = web::WebRequest();
+        m_fields->m_listener.bind([this](web::WebTask::Event* e) {
+            if (web::WebResponse* res = e->getValue()) {
+                if (res->ok() && res->header("Content-Type") == "image/png") {
+                    auto data = res->data();
+                    auto img = new CCImage();
+                    if (img->initWithImageData(data.data(), data.size())) {
+                        auto greg = new CCTexture2D();
+                        if (greg->initWithImage(img)) {
+                            auto funny = CCSprite::createWithTexture(greg);
+                            funny->setPosition({0, 0});
+                            funny->setAnchorPoint({0, 0});
+                            funny->setOpacity(0);
+                            funny->setScale(2.0f);
+                            this->addChild(funny, 10);
+                            m_fields->fetchedImage = funny;
+
+                            auto sequence = CCSequence::create(
+                                CCFadeIn::create(0.5f),
+                                CCDelayTime::create(5.0f),
+                                CCFadeOut::create(0.5f),
+                                nullptr
+                            );
+                            funny->runAction(sequence);
+                        }
+                        greg->release();
+                    }
+                    img->release();
+                } else {
+                    FLAlertLayer::create(
+                        // skill issue
+                        "Network Error",
+                        "Either you aren't connected to the internet or my website is down.",
+                        "OK"
+                    )->show();
+                }
+            }
+        });
+
+        m_fields->m_listener.setFilter(req.get("https://geometrydash.us.kg/api/bubby/"));
+
+        this->runAction(
+            CCSequence::create(
+                CCDelayTime::create(2.0f),
+                CCCallFunc::create(this, callfunc_selector(kanye::enableButton)),
+                nullptr
+            )
+        );
+    }
+
+    void enableButton() {
+        if (m_fields->billy) {
+            m_fields->billy->setEnabled(true);
+        }
+    }
+};
